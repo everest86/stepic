82cc8bc303d0a7b18b97076f7fda66404501582c
roman.chaptykov <everest86@bk.ru>
2026-02-18 16:17:10 +0700

    1

--- stepic/A_pandas/main.py
+++ stepic/A_pandas/main.py
@@ -1,9 +1,11 @@
+from xml.etree.ElementInclude import include
+
 import pandas as pd
 import numpy as np
 from scipy import stats
 
 DATASET_PATH = './housing.csv'
-PREPARED_DATASET_PATH = './housing_prepared.csv'
+PREPARED_DATASET_PATH = './advertising.csv'
 # Показать все столбцы
 pd.set_option('display.max_columns', None)
 # По желанию: увеличить ширину строки, чтобы столбцы не переносились
@@ -76,10 +78,10 @@ df = pd.read_csv(DATASET_PATH, sep=',')
 # print(df['id'].dtype)
 
 # # количественные переменные
-# df_num_features = df.select_dt ypes(include=['float64', 'int64']) # возвращает только количественные данные
+# df_num_features = df.select_dtypes(include=['float64', 'int64']) # возвращает только количественные данные
 # print(df_num_features.head())
 
-# # информация о данных
+# # информация о данных статистика
 # print(df.describe())
 #
 # print(df.info()) # информация о метаданных
@@ -102,8 +104,96 @@ a = np.array([1, 2, 2, 3, 4, 5, 5])
 # print(np.quantile(a, 0.25)) # квантиль
 
 # print(stats.mode(a)) # выводит частовстречающееся значение и сколько раз
-print(pd.Series(a).mode()) # выводит частовстречающееся значение и сколько раз (работает лучше, чем предыдущий вариант)
+# print(pd.Series(a).mode()) # выводит частовстречающееся значение и сколько раз (работает лучше, чем предыдущий вариант)
+#
+# print(df['total_rooms'].value_counts()) # количество каждого из значений
+# print(df['total_rooms'].mode()) # выводит частовстречающееся значение и сколько раз (работает лучше, чем предыдущий вариант)
+# print(df[df['total_rooms']==1527].shape) # количество строк равное значению 1527
 
-print(df['total_rooms'].value_counts()) # количество каждого из значений
-print(df['total_rooms'].mode()) # выводит частовстречающееся значение и сколько раз (работает лучше, чем предыдущий вариант)
-print(df[df['total_rooms']==1527].shape) # количество строк равное значению 1527
+# # номинативные характеристики
+# dfObjFeatures = df.select_dtypes(include=['object','string'])
+# print(dfObjFeatures.head())
+
+# print(df['ocean_proximity'].value_counts())
+# print(df['ocean_proximity'].unique()) # == df['ocean_proximity'].value_counts().index
+# print(df['ocean_proximity'].nunique()) # количество уникальных == len(df['ocean_proximity'].unique())
+
+# sort
+# print(df.tail())
+# print(df.sort_values(by='median_house_value', ascending=True))
+# print(df.sort_values(by=['median_house_value','total_rooms'], ascending=True).head(10))
+
+# merge
+df2 = pd.read_csv(PREPARED_DATASET_PATH, sep=',')
+
+df2['User_id'] = pd.Series(range(1000, 2000))
+
+cols_1 = ['User_id', 'Daily Time Spent on Site', 'Ad Topic Line', 'Timestamp', 'Clicked on Ad']
+df_site = df2[cols_1]
+
+cols_2 = ['User_id', 'Male', 'Age', 'Country', 'City', 'Daily Internet Usage']
+df_personal = df2[cols_2][:-50]
+
+cols_3 = ['Country', 'City', 'Area Income']
+
+df_macro = df2[cols_3][:-50]
+
+# merge
+# print(pd.merge(df_site, df_personal, on='User_id', how='inner'))
+# df3=df_site.merge(df_personal, left_on='User_id', right_on='User_id', how='left')
+# print(df3.isna().sum()) # частично пустых строк
+
+# # join по индексу
+# df_6 = df_site.set_index('User_id')
+# df_7 = df_personal.set_index('User_id')
+# # print(df_6)
+#
+# df9=df_6.join(df_7, how='inner')
+# print(df9.head())
+#
+# df9.reset_index(inplace=True) # сброс индекса
+# print(df9.head())
+
+# groupby
+# print(df2.head())
+# print(df2.groupby('Male')['Age'].mean())  # сначала берем нужную колонку, только потом агрегируем
+# print(df2.groupby(['Male', 'Clicked on Ad'])['Age'].mean())
+# print(df2.groupby(['Male', 'Clicked on Ad'])['Age'].mean().unstack()) # строки стали колонками
+# print(df2.groupby(['Male', 'Clicked on Ad']).agg({'Daily Internet Usage': 'mean', 'Age': ['max', 'min']}))
+#
+# print(pd.pivot_table(
+#     data=df2,
+#     columns='Clicked on Ad',
+#     values='Daily Internet Usage',
+#     aggfunc='mean'
+# ))
+#
+# print(df2.pivot_table(index='Male',
+#                 columns='Clicked on Ad',
+#                 values='Daily Internet Usage',
+#                 aggfunc='mean'))
+#
+# print(pd.pivot_table(data=df2,
+#                      values='Daily Internet Usage',
+#                      index=['Male', 'Clicked on Ad'],
+#                      columns='Country',
+#                      aggfunc='mean'))
+
+print(pd.crosstab(index=df2['Male'], columns=df2['Clicked on Ad']))
+
+print(pd.crosstab(index=df2['Male'],
+            columns=df2['Clicked on Ad'],
+            values=df2['Daily Internet Usage'],
+            aggfunc='mean'))
+
+print(pd.crosstab(index=df2['Male'],
+            columns=df2['Clicked on Ad'],
+            normalize='all')) # выводит информацию в % по всем Clicked on Ad
+
+print(pd.crosstab(index=df2['Male'],
+            columns=df2['Clicked on Ad'],
+            normalize='index')) # выводит информацию в % по каждому Clicked on Ad по строке
+
+print(pd.crosstab(index=df2['Male'],
+            columns=df2['Clicked on Ad'],
+            normalize='columns')) # выводит информацию в % по каждому Clicked on Ad по колонке
