94b93eb744408950b5276d2580c4b7e2bf47bb51
roman.chaptykov <everest86@bk.ru>
2026-03-05 13:22:38 +0700

    home2

--- stepic/B_matplotlib/home1/main.py
+++ stepic/B_matplotlib/home1/main.py
@@ -38,56 +38,54 @@ sns.boxplot(x=employees['JoiningYear'], y=employees['Education'], width=1.5)
 plt.show()
 
 # 1.5. Кто больше в среднем заработывает М или Ж
-# Вывод: с
-employees['gender_int']=0
-employees.loc[employees['Gender']=='Male', 'gender_int']=1
-a=employees.groupby('gender_int')['PaymentTier'].mean()
-print(a.values())
+# Вывод: у мужчин уровень оплаты в среднем выше
+a = employees[['Gender', 'PaymentTier']].groupby('Gender')['PaymentTier'].mean()
 plt.figure(figsize=(10, 9))
-plt.pie(a.values(), labels=a.index, autopct='%.2f%%')
+gender_map = {"1": "male", "0": "female"}
+sns.barplot(x=a.index, y=a.values)
 plt.show()
 
-# # 1. Матрица корреляций
-# # вывод: сильных зависимостей нет
-# corr_matrix = employees.select_dtypes('number').corr()
-# corr_matrix = np.round(corr_matrix, 2)
-# corr_matrix[np.abs(corr_matrix) < 0.1] = 0
-# sns.heatmap(corr_matrix, annot=True, linewidths=.5, cmap='coolwarm')
-# plt.title('Correlation matrix')
-# plt.show()
-#
-# # 2. График зависимости количества сотрудников сгруппированных по уровню образования
-# a = employees['Education'].value_counts()
-# plt.subplot(1, 3, 1)
-# sns.barplot(x=a.index, y=a.values)  # вывод: бакалавров больше
-# # plt.pie(a.values, labels=a.index, autopct='%.2f%%')
-#
-# # 3. График зависимости количества сотрудников сгруппированных по городу
-# a = employees['City'].value_counts()
-# plt.subplot(1, 3, 2)
-# sns.barplot(x=a.index, y=a.values)  # вывод: родом из Bangalore сотрудников больше
-# # plt.pie(a.values, labels=a.index, autopct='%.2f%%')
-#
-# # 4. График количества мужчин и женщин
-# a = employees['Gender'].value_counts()
-# plt.subplot(1, 3, 3)
-# sns.barplot(x=a.index, y=a.values)  # вывод: мужчин больше чем женщин
-# # plt.pie(a.values, labels=a.index, autopct='%.2f%%')
-# plt.show()
-#
-# # 5. График зависимости оплаты от образования. Вывод: уровень оплаты труда не зависит от образования
-# plt.figure(figsize=(16, 8))
-# plt.scatter(employees['PaymentTier'], employees['Education'])
-# plt.xlabel('Payment Tier')
-# plt.ylabel('Education')
-# plt.title('Distribution of PaymentTier by Education')
-# plt.show()
-#
-# # 6. График зависимости оплаты от образования.
-# # Вывод: возраст сотрудников для PaymentTier in (2,3) схожий.
-# plt.figure(figsize=(16, 8))
-# sns.boxplot(x=employees['PaymentTier'], y=employees['Age'], whis=1.5)
-# plt.xlabel('Payment Tier')
-# plt.ylabel('Age')
-# plt.title('Distribution of PaymentTier by Age')
-# plt.show()
+# 1.6. График зависимости количества сотрудников сгруппированных по уровню образования
+a = employees['Education'].value_counts()
+plt.subplot(1, 3, 1)
+sns.barplot(x=a.index, y=a.values)  # вывод: бакалавров больше
+# plt.pie(a.values, labels=a.index, autopct='%.2f%%')
+
+# 1.7. График зависимости количества сотрудников сгруппированных по городу
+a = employees['City'].value_counts()
+plt.subplot(1, 3, 2)
+sns.barplot(x=a.index, y=a.values)  # вывод: родом из Bangalore сотрудников больше
+# plt.pie(a.values, labels=a.index, autopct='%.2f%%')
+
+# 1.8. График количества мужчин и женщин
+a = employees['Gender'].value_counts()
+plt.subplot(1, 3, 3)
+sns.barplot(x=a.index, y=a.values)  # вывод: мужчин больше чем женщин
+# plt.pie(a.values, labels=a.index, autopct='%.2f%%')
+plt.show()
+
+# 1.9. График зависимости оплаты от образования. Вывод: уровень оплаты труда не зависит от образования
+plt.figure(figsize=(16, 8))
+plt.scatter(employees['PaymentTier'], employees['Education'])
+plt.xlabel('Payment Tier')
+plt.ylabel('Education')
+plt.title('Distribution of PaymentTier by Education')
+plt.show()
+
+# 1.10. График зависимости оплаты от образования.
+# Вывод: возраст сотрудников для PaymentTier in (2,3) схожий.
+plt.figure(figsize=(16, 8))
+sns.boxplot(x=employees['PaymentTier'], y=employees['Age'], whis=1.5)
+plt.xlabel('Payment Tier')
+plt.ylabel('Age')
+plt.title('Distribution of PaymentTier by Age')
+plt.show()
+
+# 2. Матрица корреляций
+# вывод: сильных зависимостей нет
+corr_matrix = employees.select_dtypes('number').corr()
+corr_matrix = np.round(corr_matrix, 2)
+corr_matrix[np.abs(corr_matrix) < 0.1] = 0
+sns.heatmap(corr_matrix, annot=True, linewidths=.5, cmap='coolwarm')
+plt.title('Correlation matrix')
+plt.show()
